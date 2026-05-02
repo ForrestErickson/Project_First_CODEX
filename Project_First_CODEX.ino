8318121c90d5b2151fbe77302e12eace24a19b7a
Forrest Lee Erickson <forresterickson@comcast.net>
2026-05-02 07:28:37 -0400

    Add initial Project_First_CODEX Blink sketch

---
+++ Project_First_CODEX.ino
@@ -0,0 +1,13 @@
+// Project_First_CODEX
+// Based on the Arduino built-in "Blink" example.
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
